6d18e9f3480508446671d5df930081dcebff4004
amachkel <amachkel@gmail.com>
2023-06-27 17:52:55 -0400

    add resolver

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,6 +4,7 @@ import { EventsListComponent } from './events/events-list.component';
 import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './errors/404.component';
 import { EventRouteActivator } from './events/event-details/event-route-activator.service';
+import { EventListResolver } from './events/events-list-resolver.service';
 
 export const appRoutes: Routes = [
   {
@@ -11,7 +12,11 @@ export const appRoutes: Routes = [
     component: CreateEventComponent,
     canDeactivate: ['canDeactivateCreateEvent'],
   },
-  { path: 'events', component: EventsListComponent },
+  {
+    path: 'events',
+    component: EventsListComponent,
+    resolve: { events: EventListResolver },
+  },
   {
     path: 'events/:id',
     component: EventDetailsComponent,
