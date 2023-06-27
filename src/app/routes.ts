9ccb889f17f85de6d5877e9fb46489644f24b726
amachkel <amachkel@gmail.com>
2023-06-27 18:17:27 -0400

    add user directory and routes, clean up imports

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { EventsListComponent } from './events/events-list.component';
-import { CreateEventComponent } from './events/create-event.component';
+import {
+  EventDetailsComponent,
+  EventsListComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver,
+} from './events/index';
 import { Error404Component } from './errors/404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventListResolver } from './events/events-list-resolver.service';
 
 export const appRoutes: Routes = [
   {
@@ -24,4 +26,8 @@ export const appRoutes: Routes = [
   },
   { path: '404', component: Error404Component },
   { path: '', redirectTo: '/events', pathMatch: 'full' },
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.module').then((m) => m.userModule),
+  },
 ];
